8b4103dd0d6259ceec6579201d3783a5abcb7cfb
Imhotep6228 <nmdllc@live.com>
2020-04-08 23:51:57 -0700

    Updates

---
+++ precache-manifest.0cf0ff611392d0d351c8ba6af9e5204a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d83145774001478fbdc5",
+    "url": "/DappUGHPage1/static/js/runtime~main.d8314577.js"
+  },
+  {
+    "revision": "3e16085af84a434cce6a",
+    "url": "/DappUGHPage1/static/js/main.3e16085a.chunk.js"
+  },
+  {
+    "revision": "6df1fb693f71828924d4",
+    "url": "/DappUGHPage1/static/js/1.6df1fb69.chunk.js"
+  },
+  {
+    "revision": "3e16085af84a434cce6a",
+    "url": "/DappUGHPage1/static/css/main.acfa1869.chunk.css"
+  },
+  {
+    "revision": "6df1fb693f71828924d4",
+    "url": "/DappUGHPage1/static/css/1.1766dd5b.chunk.css"
+  },
+  {
+    "revision": "08cf07db861a2bead599058cc01549ad",
+    "url": "/DappUGHPage1/index.html"
+  }
+];
